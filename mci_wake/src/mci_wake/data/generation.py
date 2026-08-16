15a5d99a20afb1809dde6a0779608bc5622de32f
Kiril <kirbou06012@gmail.com>
2026-08-16 15:07:32 -0300

    added train_stitch_model and signature for stitching generation

---
+++ mci_wake/src/mci_wake/data/generation.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+from mci_wake.data import RawData
+import numpy.typing as npt
+
+def generate_training_data(
+    data: RawData,
+    target_sequence: list[str],
+    n_positive_per_subject: int = 15,
+    n_hard_negative_per_subject: int = 15,
+    sampling_rate: float = 200.0
+) -> tuple[list[npt.NDArray[np.floating]], list[int]]:
+    """
+    Generates synthetic positive and hard negative sequence trials for given subjects.
+    Stitching is done strictly within the same subject to model realistic intra-user dynamics.
+    """
+    # need to normalize pre-stitching
+    pass
--- mci_wake/src/mci_wake/data/types.py
+++ mci_wake/src/mci_wake/data/types.py
@@ -31,6 +31,37 @@ class RawData:
     def __iter__(self):
         return iter((self.epn_emg, self.epn_labels, self.epn_subjects, self.adl_emg, self.adl_subjects))
 
+    def split(self, test_percentage: float) -> tuple["RawData", "RawData"]:
+        unique_subjects = np.unique(self.epn_subjects)
+
+        n_test = int(len(unique_subjects) * test_percentage)
+        test_subject_ids = list(unique_subjects[:n_test])
+
+        epn_test_mask = np.isin(self.epn_subjects, test_subject_ids)
+        epn_train_mask = ~epn_test_mask
+
+        unique_subjects = np.unique(self.adl_subjects)
+        n_test = int(len(unique_subjects) * test_percentage)
+        test_subject_ids = list(unique_subjects[:n_test])
+        adl_test_mask = np.isin(self.adl_subjects, test_subject_ids)
+        adl_train_mask = ~adl_test_mask
+
+        test = RawData(
+            epn_emg=self.epn_emg[epn_test_mask],
+            epn_labels=self.epn_labels[epn_test_mask],
+            epn_subjects=self.epn_subjects[epn_test_mask],
+            adl_emg=self.adl_emg[adl_test_mask],
+            adl_subjects=self.adl_subjects[adl_test_mask]
+        )
+        train = RawData(
+            epn_emg=self.epn_emg[epn_train_mask],
+            epn_labels=self.epn_labels[epn_train_mask],
+            epn_subjects=self.epn_subjects[epn_train_mask],
+            adl_emg=self.adl_emg[adl_train_mask],
+            adl_subjects=self.adl_subjects[adl_train_mask]
+        )
+        return train, test
+
 
 gesture_mapping: Dict[str, int] = {'noGesture': 0, 'fist': 1, 'waveIn': 2, 'waveOut': 3, 'open': 4, 'pinch': 5}
 
